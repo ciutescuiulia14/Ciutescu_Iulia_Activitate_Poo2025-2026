9d3fc8acc4f7745e756672cd292d4cc83e01a019
Ciutescu Iulia Alexandra <ciutescuiulia14@stud.ase.ro>
2025-11-23 17:26:33 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Factura {
+private:
+	const int id;
+	static int nrFacturi;
+
+
+	string client;
+
+	int nrPozitii;
+	float* valori;
+public:
+	//constructor implicit
+	Factura() : id(++nrFacturi) {
+		this->client = "Anonim";
+		this->nrPozitii = 0;
+		this->valori = nullptr;
+	}
+	//constructor cu toti parametrii
+	Factura(string _client, int nrPozitii, float* valori) : id(++nrFacturi) {
+		client = _client;
+		this->nrPozitii = nrPozitii;
+		if (nrPozitii > 0 && valori != nullptr) {
+			this->valori = new float[nrPozitii];
+			for (int i = 0; i < nrPozitii; i++) {
+				this->valori[i] = valori[i];
+			}
+		}
+		else
+		{
+			this->valori = nullptr;
+		}
+		delete[]valori;
+	}
+
+
+	const int getId() {
+		return this->id;
+	}
+
+	static int getNrFacturi() {
+		return nrFacturi;
+	}
+
+	static void setNrFacturi(int _nrFacturi) {
+		nrFacturi = _nrFacturi;
+	}
+
+	string getClient() {
+		return this->client;
+	}
+
+	void setClient(int client) {
+		this->client = client;
+	}
+
+	int getNrPozitii() {
+		return this->nrPozitii;
+	}
+
+	void setNrPozitii(int nrPozitii) {
+		if (nrPozitii > 0) {
+			this->nrPozitii = nrPozitii;
+		}
+		else {
+			throw "Numar incorect";
+		}
+	}
+
+	float* getValori() {
+		float* vectorValori = new float[this->nrPozitii];
+		for (int i= 0; i < nrPozitii; i++) {
+			vectorValori[i] = this->valori[i];
+		}
+		return vectorValori;
+	}
+
+	void setValori(float* noulVectorValori, int nouaDimensiune) {
+		setNrPozitii(nouaDimensiune);
+		if (this->valori != nullptr) {
+			delete[] this->valori;
+		}
+		this->valori = new float[nouaDimensiune];
+		for (int i = 0; i < nouaDimensiune; i++) {
+			this->valori[i] = noulVectorValori[i];
+		}
+		delete[] noulVectorValori;
+	}
+
+	~Factura() {
+		cout << endl << "s-a apelat destructorul";
+		if (this->valori != nullptr)
+			delete[] this->valori;
+	}
+
+	void afisareFactura() {
+		cout << this->id << ".Factura pentru clientul  " << this->client;
+		if(this->nrPozitii > 0){
+			cout << " are " << this->nrPozitii << " pozitii cu valorile: ";
+			if (this->valori != nullptr) {
+				for (int i = 0; i < this->nrPozitii; i++) {
+					cout << "  " << this->valori[i];
+				}
+			}
+		}
+		cout << endl;
+	}
+
+
+};
+
+int Factura::nrFacturi = 0;
+
+int main() {
+	Factura f1;
+	cout << endl << " Noua valoare este: " << f1.getNrPozitii();
+	f1.afisareFactura();
+
+	Factura f2("Anonim", 3, new float[3] {300, 450, 50.2});
+	f2.afisareFactura();
+	f2.setValori(new float[2] {75, 130}, 2);
+	
+}
